710456d651954c49a3ad1554a53c68ebc71a4a7a
pirhoo <pierre.romera@gmail.com>
2015-06-07 17:21:04 +0200

    Added base url

--- src/app/index.constant.js
+++ src/app/index.constant.js
@@ -5,11 +5,11 @@ angular.module('tmfMap')
     fallbackLanguage: ['en_US'],
     languages: {
       'en_US': {
-        APP_TITLE: 'The Migrants\' Files',
+        APP_TITLE: 'The Migrants\' Files - EN',
         INFOWINDOW_CONTENT: 'Hello world'
       },
       'fr_FR': {
-        APP_TITLE: 'The Migrants\' Files',
+        APP_TITLE: 'The Migrants\' Files - FR',
         INFOWINDOW_CONTENT: 'Bonjour le monde'
       }
     }
