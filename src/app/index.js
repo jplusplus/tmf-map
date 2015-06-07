8bde2107773ec2bbe2746690579b031e8b3dea2c
pirhoo <pierre.romera@gmail.com>
2015-06-07 15:37:23 +0200

    :alien:

---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('tmfMap', ['ngAnimate', 'ngTouch', 'ngSanitize']);
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('tmfMap')
+  .controller('MainCtrl', function ($scope) {
+  });
