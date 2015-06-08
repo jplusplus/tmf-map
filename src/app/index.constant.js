781652a8089e79938a03fffda6fc08b0ac7d562f
Nicolas Kayser-Bril <n.kayserbril@gmail.com>
2015-06-08 16:21:19 +0200

    added FR & DE

--- src/app/index.constant.js
+++ src/app/index.constant.js
@@ -6,11 +6,15 @@ angular.module('tmfMap')
     languages: {
       'en_US': {
         APP_TITLE: 'The Migrants\' Files - EN',
-        INFOWINDOW_CONTENT: '{{ deaths }} refugees and migrants died at this location between 2000 and April 20,2015.'
+        INFOWINDOW_CONTENT: '{{ deaths }} refugees and migrants died at this location between 2000 and June 18,2015.'
       },
       'fr_FR': {
         APP_TITLE: 'The Migrants\' Files - FR',
-        INFOWINDOW_CONTENT: '{{ deaths }} refugees and migrants died at this location between 2000 and April 20,2015.'
+        INFOWINDOW_CONTENT: '{{ deaths }} réfugiés et migrants sont morts à cet endroit entre l\'an 2000 et le 18 juin 2015.'
+      },
+      'de_DE': {
+        APP_TITLE: 'The Migrants\' Files - DE',
+        INFOWINDOW_CONTENT: '{{ deaths }} Flüchtlinge und Migranten sind an dieser Stelle zwischen 2000 und den 18.6.2015 gestorben.'
       }
     }
   });
