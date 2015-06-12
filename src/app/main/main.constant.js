bcbba8250f616a94cdcdf0daa9f073ce7191e97a
Nicolas Kayser-Bril <n.kayserbril@gmail.com>
2015-06-12 12:54:13 +0200

    Changed viz URL

--- src/app/main/main.constant.js
+++ src/app/main/main.constant.js
@@ -3,6 +3,6 @@
 angular.module('tmfMap')
   .constant('main', {
     interactivity: ['cartodb_id', 'deaths', 'lat', 'long'],
-    viz: 'http://jplusplus.cartodb.com/api/v2/viz/9284b33a-e903-11e4-8de1-0e0c41326911/viz.json',
+    viz: 'http://jplusplus.cartodb.com/api/v2_1/viz/22a9ba36-10f1-11e5-91e1-0e0c41326911/viz.json',
     dataset: 'https://jplusplus.cartodb.com/tables/the_migrants_files_refugees_and_migrants_who_d/public'
   });
